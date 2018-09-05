1d9d92fd24d5da8796954a0ba077d1099c753566
prakharg24 <mohammadali.khan13@gmail.com>
2018-09-05 12:46:19 +0530

    KMeans clustering

--- BloodTest.py
+++ BloodTest.py
@@ -18,10 +18,38 @@ def dataProcessing(d,k):
 	return [xtrain,ytrain,xtest,ytest,x]
 
 
+def DataVisualization(x):
+	plt.figure(0)
+	marker=['o','+','^','x','D','*','h','8','p','s','|','_']
+	for i in range(0,k):
+		plt.scatter(x[i][:,0],x[i][:,1],marker=marker[i])
+	plt.show()
+
+
 def MLEGaussian(x):
 	mean = np.mean(x,axis=0)
 	var = np.var(x,axis=0)
 	return [mean,var]
+
+def distanceMetric(x1,x2):
+	x1=np.asarray(x1)
+	x2=np.asarray(x2)
+	m=np.size(x1,axis=0)
+	n=np.size(x2,axis=0)
+	d=np.size(x1,axis=1)
+	x1=np.reshape(x1,(1,m,d))
+	x2=np.reshape(x2,(n,1,d))
+	diff = x1-x2
+	diff=diff*diff
+	res = np.sum(diff,axis=2)
+	return res # every row corresponds to different test sample, every column represents training sample
+
+def calculateAccuracy(ycalc,ytest):
+	err = ycalc-ytest
+	err = np.where(err==0)
+	acc = np.size(err)*1.0/np.size(ycalc)*100
+	return acc
+
 
 def Naiivegaussian(x,mu,var):
 	d = np.size(x,axis=1)
@@ -58,13 +86,6 @@ def classifyNaiiveBayes(x,xtest,ytest,N):
 	print "Accuracy on test set for naiive bayes classifier is ",acc
 
 
-def calculateAccuracy(ycalc,ytest):
-	err = ycalc-ytest
-	err = np.where(err==0)
-	acc = np.size(err)*1.0/np.size(ycalc)*100
-	return acc
-
-
 
 def classifyKNN(xtrain,ytrain,xtest,ytest, knn):
 	n = np.size(xtest,axis=0)
@@ -85,52 +106,58 @@ def classifyKNN(xtrain,ytrain,xtest,ytest, knn):
 	print "Accuracy for k-nearest neighbour with k = ",knn," is ",acc
 	return acc
 
-
-
-
+def kMeans(xtrain,km):
+	theta = MLEGaussian(xtrain)
+	var = theta[1]
+	mu = theta[0]
+	rnd = np.random.rand(km,np.size(xtrain,axis=1))
+	centroid = var*rnd+mu
 	
+	c=0
+	while(True):
 
-def distanceMetric(x1,x2):
-	x1=np.asarray(x1)
-	x2=np.asarray(x2)
-	m=np.size(x1,axis=0)
-	n=np.size(x2,axis=0)
-	d=np.size(x1,axis=1)
-	x1=np.reshape(x1,(1,m,d))
-	x2=np.reshape(x2,(n,1,d))
-	diff = x1-x2
-	diff=diff*diff
-	res = np.sum(diff,axis=2)
-	return res # every row corresponds to different test sample, every column represents training sample
-
-
-
-
-
-def DataVisualization(x):
-	plt.figure(0)
-	marker=['o','+','^','x','D','*','h','8','p','s','|','_']
-	for i in range(0,k):
-		plt.scatter(x[i][:,0],x[i][:,1],marker=marker[i])
-	plt.show()
+		prev=np.copy(centroid)
+		dist = distanceMetric(xtrain,centroid)
+		curr = np.argmin(dist,axis=0)
+		for i in range(0,km):
+			temp=np.where(curr==i)
+			x1=xtrain[temp]
+			centroid[i]=np.mean(x1,axis=0)
+		update=distanceMetric(prev,centroid)
+		update=update*np.eye(km,km)
+		update=np.sum(update,axis=0)
+		mindiff=np.min(update)
+		kMeansVisualisation(xtrain,centroid,km,c)
+		c+=1
+		print c
+		print mindiff
+		if(mindiff<=0.000001):
+			break;
 
 
-
-d = genfromtxt('medicalData.txt')
-k = 3 # Number of classes
-dp = dataProcessing(d,k)
-xtrain = dp[0]
-ytrain = dp[1]
-xtest = dp[2]
-ytest = dp[3]
-x = dp[4]
-N = np.size(xtrain,axis=0)
+	plt.clf()
+	plt.scatter(xtrain[:,0],xtrain[:,1],color='grey',s=2)
+	plt.savefig('unclustered.png')
 
 
 
-# DataVisualization(x)
-knn= 10 # number of nearest neighbours to be considered
-classifyNaiiveBayes(x,xtest,ytest,N)
+	# print centroid
+
+
+def kMeansVisualisation(xtrain,centroid,km,i1):
+	plt.figure(1)
+	dist = distanceMetric(xtrain,centroid)
+	curr = np.argmin(dist,axis=0)
+	color=['red','blue','green','yellow','orange','purple','black','pink','aqua']
+	for i in range(0,km):
+		temp=np.where(curr==i)
+		x1=xtrain[temp]
+		plt.scatter(centroid[i][0],centroid[i][1],s=100,color='black',marker='X')
+		plt.scatter(x1[:,0],x1[:,1],s=2,color=color[i])
+	name=str(i1)+'.png'
+	print name
+	plt.savefig(name)
+	plt.clf()
 
 
 def OptimumKNN(xtrain,ytrain,xtest,ytest):
@@ -150,7 +177,26 @@ def OptimumKNN(xtrain,ytrain,xtest,ytest):
 
 
 
-OptimumKNN(xtrain,ytrain,xtest,ytest)
+
+
+d = genfromtxt('medicalData.txt')
+k = 3 # Number of classes
+dp = dataProcessing(d,k)
+xtrain = dp[0]
+ytrain = dp[1]
+xtest = dp[2]
+ytest = dp[3]
+x = dp[4]
+N = np.size(xtrain,axis=0)
+
+
+
+knn= 10 
+# classifyNaiiveBayes(x,xtest,ytest,N)
+
+# OptimumKNN(xtrain,ytrain,xtest,ytest)
+
+kMeans(d[:,1:],4)
 
 
 
